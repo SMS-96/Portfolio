497613b5141f670b962bd438ba1a5e821a282220
SMS-96 <151044318+SMS-96@users.noreply.github.com>
2024-07-28 20:56:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+// animation titre-accueil
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: (el, i) => 45 * i
+  }).add({
+    targets: '.ml10',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+//menu mobile
+function menuMobile (){
+  const btn = document.querySelector('.burger');
+  const header =document.querySelector('.header');
+  const links =document.querySelectorAll('.navbar a');
+
+  
+
+  btn.addEventListener('click',() =>{
+    header.classList.toggle('show_nav');
+  })
+  links.forEach(link => {
+    link.addEventListener('click',() => {
+      header.classList.remove('show_nav');
+    })
+  } )
+  
+}
+menuMobile();
+
+// details du portfolio
+function showProjectDetails() {
+  const links = document.querySelectorAll('.card__link');
+  const modals = document.querySelectorAll('.modal');
+  const btns = document.querySelectorAll('.modal__close');
+
+  const hideModals = () => {
+    modals.forEach(modal => {
+      modal.classList.remove('show');
+    });
+  }
+
+  links.forEach(elem => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      document.querySelector(`[id=${elem.dataset.id}]`).classList.add('show');
+    });
+  });
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+      hideModals();
+    });
+  });
+
+}
+
+showProjectDetails();
+
+// effets
+
+const observerIntersectionAnimation = () => {
+  const sections = document.querySelectorAll('section');
+  const skills = document.querySelectorAll('.skills .bar');
+
+  sections.forEach((section, index) => {
+    if (index === 0) return;
+    section.style.opacity = "0";
+    section.style.transition = "all 1.6s";
+  });
+
+  skills.forEach((elem, index) => {
+
+    elem.style.width = "0";
+    elem.style.transition = "all 1.6s";
+  });
+
+  let sectionObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.opacity = 1;
+      }
+    });
+  });
+
+  sections.forEach(section => {
+    sectionObserver.observe(section);
+  });
+
+  let skillsObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.width = elem.dataset.width + '%';
+      }
+    });
+  });
+
+  skills.forEach(skill => {
+    skillsObserver.observe(skill);
+  });
+}
+
+observerIntersectionAnimation();
+
